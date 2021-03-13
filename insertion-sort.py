2fff6f7df987c051acc11ffe43d5bce8c4bee374
Konstantin Astafurov <40547702+konstantin-aa@users.noreply.github.com>
2021-03-13 14:37:34 -0500

    uploading my insertion sort program from repl

---
+++ insertion-sort.py
@@ -0,0 +1,25 @@
+import random
+#tester
+list0 = [1,2,3,4,5,6,7]
+random.shuffle(list0)
+print(list0)
+
+#insert selected object into the sorted list
+def addIn(index0, processed):
+  index = 0
+  for item in processed:
+    if item > index0:
+      processed.insert(index,index0)
+      return processed
+    index += 1
+  processed.append(index0)
+  return processed
+
+#loop for inserting
+def insertionSort(list0):
+  temp = []
+  for item in list0:
+    temp = addIn(item,temp)
+  return temp
+
+print(insertionSort(list0))
